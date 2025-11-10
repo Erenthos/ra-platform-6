6872df82b2a13fe199dba2a1b93416e96a08a10e
Erenthos <erenthos@gmail.com>
2025-11-10 13:06:22 +0530

    Update Input.tsx

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,1 +1,21 @@
+"use client";
+
+import React from "react";
+import { cn } from "@/lib/utils";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+}
+
+export function Input({ className, ...props }: InputProps) {
+  return (
+    <input
+      {...props}
+      className={cn(
+        "w-full px-4 py-2.5 rounded-xl bg-white/10 border border-white/20 text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 transition-all duration-200",
+        className
+      )}
+    />
+  );
+}
 
